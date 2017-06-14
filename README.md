b471600b9c089cd6975fa9aea9a010b1be01269d
Evgeny Ukhanov <mrlsd@ya.ru>
2017-06-14 12:11:24 +0300

    Added template and HTTP serve handler

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# simple-http-files
+# Simple HTTP files
+Delivery files via http request
+
+Simple, fast, efficient.
+Based on `go ecbo framework`
+
+License MIT
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"html/template"
@@ -13,33 +12,37 @@ import (
 const PATH = "/home/evgeny/d/Видео/Молодой Папа  (Season 01) BaibaKo 720/"
 
 func main() {
+	e := echo.New()
+	e.Renderer = &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/**/", getMainfunc)
+	e.Static("/assets", PATH)
+	e.Logger.Fatal(e.Start(":3040"))
+}
+
+func getMainfunc(c echo.Context) error {
 	files, err := ioutil.ReadDir(PATH)
 	if err != nil {
 		panic(err)
 	}
 
+	type FileList struct {
+		Name string
+		Path string
+	}
+	var filesList []FileList
 	for _, file := range files {
-		fmt.Println(file.Name())
+		fileData := FileList{
+			Name: file.Name(),
+			Path: "/assets/" + file.Name(),
+		}
+		filesList = append(filesList, fileData)
 	}
-
-	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
-
-	e := echo.New()
-	e.Renderer = t
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/**/", func(c echo.Context) error {
-
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
-	e.Static("/assets/", PATH)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":304" +
-		"0"))
+	return c.Render(http.StatusOK, "index", filesList)
 }
 
 type Template struct {
